88a571dfc683d53fc80826ac39b9d4e62e45b431
Soori <soorichu@gmail.com>
2021-05-14 12:01:39 +0900

    Initial commit

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+    public float dist = 7.0f;
+    public float height = 3.0f;
+
+    void LateUpdate()
+    {
+        transform.position = target.position
+                            - (target.forward * dist)
+                            + (Vector3.up * height);
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float translation;
+    private float rotation;
+
+    public float tspeed = 10f;
+    public float rspeed = 100f;
+    void Update()
+    {
+        translation = Input.GetAxis("Vertical") * tspeed;
+        rotation = Input.GetAxis("Horizontal") * rspeed;
+
+        transform.Translate(0, 0, translation * Time.deltaTime);
+        transform.Rotate(0, rotation * Time.deltaTime, 0);
+    }
+}
