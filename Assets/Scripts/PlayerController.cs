87b99f4a3074c7c3e6ab046a00dfd2e2b95dc48f
Soori <soorichu@gmail.com>
2021-05-25 13:34:58 +0900

    .

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,12 @@ public class PlayerController : MonoBehaviour
 
     public float tspeed = 10f;
     public float rspeed = 100f;
+
+    private Animator ani;
+    private void Start() 
+    { 
+        ani = GetComponent<Animator>(); 
+    }
     void Update()
     {
         translation = Input.GetAxis("Vertical") * tspeed;
@@ -16,5 +22,23 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(0, 0, translation * Time.deltaTime);
         transform.Rotate(0, rotation * Time.deltaTime, 0);
+
+        if (translation != 0 || rotation != 0) 
+        { 
+            ani.SetBool("walk", true); 
+        } 
+        else 
+        {
+            ani.SetBool("walk", false); 
+        }
     }
+
+    private void OnCollisionEnter(Collision collision) 
+    { 
+        if (collision.collider.tag == "Obstacle") 
+        { 
+            ani.SetTrigger("damage"); 
+        } 
+    }
+
 }
